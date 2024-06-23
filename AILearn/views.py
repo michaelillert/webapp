248008d853292069628431dcf86356b9685cf57d
michael <michael@michaelillert.com>
2024-06-23 08:25:40 +0200

    first commit

--- AILearn/views.py
+++ AILearn/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def hello_world(request):
+    return HttpResponse("Hello, World! This is a Python web app using Django.")
